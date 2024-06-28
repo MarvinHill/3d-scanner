43ff8cf3c8bcba51b74b50ff2ffc691dd6407583
Marvin Hill <86198682+MarvinHill@users.noreply.github.com>
2024-06-28 16:47:59 +0200

    feat: added config and gitignore

---
+++ frontend-scanner/vite.config.js
@@ -0,0 +1,26 @@
+import { build } from "vite";
+
+// vite.config.js
+export default {
+    // config options
+    build: {
+      rollupOptions: {
+        output: {
+          // Define the entry point for the CSS
+          entryFileNames: 'assets/[name].js',
+          chunkFileNames: 'assets/[name].js',
+          assetFileNames: ({ name }) => {
+            // This condition ensures that CSS files are moved to the root directory
+            if (name && name.endsWith('.css')) {
+              return '[name][extname]';
+            }
+            return 'assets/[name][extname]';
+          }
+        }
+      },
+      // Directory where Vite will output the built files
+      outDir: 'dist',
+      // Ensure that the assets are placed in the same directory
+      assetsDir: '.'
+    }
+  }
