fe32226fcc34564bb285164fa68f71a660946dff
MarvinHill <86198682+MarvinHill@users.noreply.github.com>
2024-06-28 11:24:32 +0200

    feat: updated scanner init

--- firmware-rpi/internal/scanner.go
+++ firmware-rpi/internal/scanner.go
@@ -5,16 +5,45 @@ import (
 	"gobot.io/x/gobot/v2/platforms/raspi"
 )
 
+type PhotoRequest struct {
+	angleCameraAxis int
+	angleTableAxis  int
+}
+
+type PhotoJob struct {
+	PhotoRequest *[]PhotoRequest
+}
+
 type ScannerDriver struct {
 	Adapter          *raspi.Adaptor
 	MotorOneDriver   *gpio.StepperDriver
 	MotorTwoDriver   *gpio.StepperDriver
 	MotorThreeDriver *gpio.StepperDriver
+	currentPosition  []int
+	PhotoJobs        chan PhotoJob
+	JobResultChannel chan []byte
 }
 
-func (s *ScannerDriver) Run() {
+func NewScannerDriver() *ScannerDriver {
+	s := &ScannerDriver{}
 	s.Adapter = raspi.NewAdaptor()
 	s.MotorOneDriver = gpio.NewStepperDriver(s.Adapter, [4]string{"8", "9", "7", "0"}, gpio.StepperModes.SinglePhaseStepping, 2048)
 	s.MotorTwoDriver = gpio.NewStepperDriver(s.Adapter, [4]string{"2", "3", "12", "13"}, gpio.StepperModes.SinglePhaseStepping, 2048)
 	s.MotorThreeDriver = gpio.NewStepperDriver(s.Adapter, [4]string{"15", "16", "1", "4"}, gpio.StepperModes.SinglePhaseStepping, 2048)
+
+	s.PhotoJobs = make(chan PhotoJob, 100)
+	s.JobResultChannel = make(chan []byte)
+
+	return s
+}
+
+func (s *ScannerDriver) LevelSites() {
+	// Level the scanner Sites
+}
+
+func (s *ScannerDriver) Run() {
+	for true {
+		//Check if there are jobs in the queue
+	}
+
 }
